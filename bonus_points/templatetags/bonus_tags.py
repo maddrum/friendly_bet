36b0af20868940923bae85cea351deb2917a5975
maddrum <maddrum9@gmail.com>
2021-06-15 22:04:00 +0300

    WiP - bonus banner updates

--- bonus_points/templatetags/bonus_tags.py
+++ bonus_points/templatetags/bonus_tags.py
@@ -10,5 +10,5 @@ from bonus_points.models import BonusDescription
 @register.simple_tag
 def check_for_new_bonuses():
     now_minus_days = timezone.now() - datetime.timedelta(days=2)
-    bonus_check = BonusDescription.objects.filter(created_on__gte=now_minus_days).exists()
+    bonus_check = BonusDescription.objects.filter(created_on__gte=now_minus_days, bonus_active=True).exists()
     return bonus_check
