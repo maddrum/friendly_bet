c8e8459e3ff53042482b3b78e22a76fac9ddfbc7
maddrum <maddrum9@gmail.com>
2021-06-15 21:53:51 +0300

    WiP - bonus banner

---
+++ bonus_points/templatetags/bonus_tags.py
@@ -0,0 +1,14 @@
+import datetime
+
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+from bonus_points.models import BonusDescription
+
+
+@register.simple_tag
+def check_for_new_bonuses():
+    now_minus_days = timezone.now() - datetime.timedelta(days=2)
+    bonus_check = BonusDescription.objects.filter(created_on__gte=now_minus_days).exists()
+    return bonus_check
